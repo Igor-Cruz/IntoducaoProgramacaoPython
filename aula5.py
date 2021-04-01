b12eb119608b5e89bfc615d05647fdf4763a3e92
Igor-Cruz <igorcruz.it@outlook.com>
2021-04-01 12:27:30 -0300

    Aprendendo a utlizar listas e tuplas

---
+++ aula5.py
@@ -0,0 +1,26 @@
+tupla =tuple(1,1,23,44,56)
+
+
+
+# lista = [1,3,5,7]
+# lista_animal = ['gato', 'cachorro', 'leão']
+
+# print(lista_animal[1])
+
+# if 'lobo' in lista_animal:
+#     print('Tem esse animal aqui')
+# else:
+#     print('Não existe relação desse animal, porém será incluido')
+#     lista_animal.append('lobo')
+#     print(lista_animal)
+
+# lista_animal.remove('gato')
+# print(lista_animal)
+
+# lista_animal.pop(1)
+# print(lista_animal)
+
+# print(sum(lista))
+# print(max(lista))
+# print(min(lista))
+
